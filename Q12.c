2a577d61858e7ae3c8a2cd4df0250aa0021ac5c2
SurajBahirwade <156635785+SurajBahirwade@users.noreply.github.com>
2026-04-27 17:54:09 +0530

    Add page replacement algorithm in Q12.c
    Implement a page replacement algorithm to track page faults and display frame status.

---
+++ Q12.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+    int n, f, i, j, k, pages[100], frame[50];
+    int faults = 0, index = 0;
+
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
+
+    printf("Enter page reference string:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &pages[i]);
+    }
+
+    printf("Enter number of frames: ");
+    scanf("%d", &f);
+
+    for(i = 0; i < f; i++) {
+        frame[i] = -1;
+    }
+
+    for(i = 0; i < n; i++) {
+        int found = 0;
+
+        for(j = 0; j < f; j++) {
+            if(frame[j] == pages[i]) {
+                found = 1;
+                break;
+            }
+        }
+
+        if(!found) {
+            frame[index] = pages[i];
+            index = (index + 1) % f;
+            faults++;
+        }
+
+        printf("Frames: ");
+        for(k = 0; k < f; k++) {
+            if(frame[k] != -1)
+                printf("%d ", frame[k]);
+            else
+                printf("- ");
+        }
+        printf("\n");
+    }
+
+    printf("Total Page Faults = %d\n", faults);
+
+    return 0;
+}
