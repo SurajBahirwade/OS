da0ef57a2aa1eabe755544b92043a4cc6969385a
SurajBahirwade <156635785+SurajBahirwade@users.noreply.github.com>
2026-04-27 18:41:03 +0530

    Add process handling and sorting in Q22.c

---
+++ Q22.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void sort(int a[], int n) {
+    int i, j, t;
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(a[j] > a[j + 1]) {
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }
+        }
+    }
+}
+
+void display(int a[], int n) {
+    int i;
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    pid_t pid = fork();
+
+    if(pid > 0) {
+        printf("\nParent Process\n");
+        sort(arr, n);
+        printf("Sorted by Parent: ");
+        display(arr, n);
+        wait(NULL);
+        printf("Child finished\n");
+    }
+    else if(pid == 0) {
+        printf("\nChild Process\n");
+        sort(arr, n);
+        printf("Sorted by Child: ");
+        display(arr, n);
+        exit(0);
+    }
+    else {
+        printf("Fork failed\n");
+    }
+
+    pid_t z = fork();
+
+    if(z > 0) {
+        sleep(10);
+    }
+    else if(z == 0) {
+        exit(0);
+    }
+
+    pid_t o = fork();
+
+    if(o > 0) {
+        exit(0);
+    }
+    else if(o == 0) {
+        sleep(5);
+        printf("Orphan Child New Parent PID: %d\n", getppid());
+    }
+
+    return 0;
+}
